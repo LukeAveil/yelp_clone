04418503f30992fe59b3dbcb173613dd8e260c8d
Luke <lukeaveil@yahoo.com>
2016-07-12 12:04:33 +0100

    create new restaurant

--- yelp_clone/spec/features/restaurants_feature_spec.rb
+++ yelp_clone/spec/features/restaurants_feature_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 feature 'restaurants have been added' do
+
   context 'no restaurants have been added' do
+
     before do
       Restaurant.create(name: 'KFC')
     end
@@ -16,6 +18,18 @@ feature 'restaurants have been added' do
       visit '/restaurants'
       expect(page).to have_content('KFC')
       expect(page).not_to have_content('No restaurants yet')
+    end
+  end
+
+  context 'creating restaurants' do
+
+    scenario 'prompts user to fill out a form, then displays the new restaurant' do
+      visit '/restaurants'
+      click_link 'Add a restaurant'
+      fill_in 'Name', with: 'KFC'
+      click_button 'Create Restaurant'
+      expect(page).to have_content 'KFC'
+      expect(current_path).to eq '/restaurants'
     end
   end
 end
